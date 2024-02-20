b5e6dab20974b9332d2060567d1630fc1c5d5cd8
Eddy1108 <edvardh@hotmail.no>
2024-02-20 09:14:00 +0100

    Update fizzbuzz.js

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,12 @@ const answer = []
 
 // Write your code below this line
 
-
-
-
+for (let index = 1; index < 16; index++) {
+    if(index % 5 === 0 & index % 3 === 0) answer.push("FizzBuzz")
+    else if(index % 3 === 0) answer.push("Fizz")
+    else if(index % 5 === 0) answer.push("Buzz")
+    else answer.push(index)    
+}
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
